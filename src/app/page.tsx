01c0828f0bbe84e31e6f8958e8128fe93ac6e821
asdimitrov13 <discopigeon13@gmail.com>
2024-06-14 16:46:16 +0100

    Basic layout and improved build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/c202cdae-fd99-421c-9d1e-f439afa3132b-td1blb.JPG",
+  "https://utfs.io/f/eca645e9-d5b0-44ae-bd96-873a4d193c8a-lkl2m7.jpg"
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello gallery in progress
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
